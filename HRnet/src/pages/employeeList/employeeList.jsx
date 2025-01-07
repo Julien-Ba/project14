7f6e813001f2a7feb168528939fa775e4e10fa59
julien-ba <julienbarbier42@gmail.com>
2025-01-07 16:58:43 +0100

    feat(employeeList): basic init

--- HRnet/src/pages/employeeList/employeeList.jsx
+++ HRnet/src/pages/employeeList/employeeList.jsx
@@ -1,5 +1,14 @@
 import './employeeList.scss';
+import { Link } from 'react-router-dom';
 
 export default function EmployeeList() {
-    return <main>EmployeeList</main>;
+    return (
+        <main className='employee-list'>
+            <h1 className='employee-list__title'>Current Employees</h1>
+            <table className='employee-list__table' id='employee-table'></table>
+            <Link className='employee-list__link' to='/'>
+                Home
+            </Link>
+        </main>
+    );
 }

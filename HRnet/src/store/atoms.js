b8c5799c71d9cc04f09a5736b0cc3fa64f4c17a5
julien-ba <julienbarbier42@gmail.com>
2025-01-13 22:51:25 +0100

    fix(states): removed log

--- HRnet/src/store/atoms.js
+++ HRnet/src/store/atoms.js
@@ -22,10 +22,4 @@ export const formDataAtom = atom({ ...privateDefaultFormData });
 export const formErrorAtom = atom({});
 
 const employeeList = await employeeService.getAllEmployees();
-if (employeeList.success) {
-    console.log('Employees found:', employeeList.data);
-} else {
-    console.error('Error:', employeeList.error);
-}
-
 export const employeeListAtom = atom(employeeList.data);

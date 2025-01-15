f098efd41e41e773b8e968ed9f690437c2dddd3f
julien-ba <julienbarbier42@gmail.com>
2025-01-15 12:12:45 +0100

    feat(employeeList): modularized table

---
+++ HRnet/src/components/table/subcomponents/TablePagination.jsx
@@ -0,0 +1,43 @@
+import './tablePagination.scss';
+import PropTypes from 'prop-types';
+
+export default function TablePagination({
+    currentPage,
+    totalPages,
+    itemsPerPage,
+    totalItems,
+    onPageChange,
+}) {
+    return (
+        <div className='table__pagination'>
+            <div className='table__pagination-info'>
+                Showing {(currentPage - 1) * itemsPerPage + 1} to{' '}
+                {Math.min(currentPage * itemsPerPage, totalItems)} of {totalItems} results
+            </div>
+            <div className='table__pagination-controls'>
+                <button
+                    onClick={() => onPageChange((prev) => Math.max(prev - 1, 1))}
+                    disabled={currentPage === 1}
+                    className='table__pagination-button'
+                >
+                    Previous
+                </button>
+                <button
+                    onClick={() => onPageChange((prev) => Math.min(prev + 1, totalPages))}
+                    disabled={currentPage === totalPages}
+                    className='table__pagination-button'
+                >
+                    Next
+                </button>
+            </div>
+        </div>
+    );
+}
+
+TablePagination.propTypes = {
+    currentPage: PropTypes.number.isRequired,
+    totalPages: PropTypes.number.isRequired,
+    itemsPerPage: PropTypes.number.isRequired,
+    totalItems: PropTypes.number.isRequired,
+    onPageChange: PropTypes.func.isRequired,
+};
